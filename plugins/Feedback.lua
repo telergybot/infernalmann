435c9a57453521d7dffaa08b93f388c32643517f
amirreza <negahapp@chmail.ir>
2016-01-15 12:01:04 +0000

    Update Feedback.lua

--- plugins/Feedback.lua
+++ plugins/Feedback.lua
@@ -19,9 +19,9 @@ local fuses = '!printf user#id' .. msg.from.id
  else
 
 
-                 local sends0 = send_msg('chat#59845789', fuse, ok_cb, false)
+                 local sends0 = send_msg('chat#76303145', fuse, ok_cb, false)
 
- return 'Your request has been sended to @Arashinfernal 😜!'
+ return 'پیام شما ارسال شد 😜!'
 
      
 
